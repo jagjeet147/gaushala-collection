246c17b2a7e32e92da25e2aff49bd9ac330342a0
jagjeet147 <jagjeet695446@gmail.com>
2026-01-27 09:53:10 +0530

    Initial commit (clean)

---
+++ admin-web/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <div className=' flex bg-gray-900 justify-center items-center h-screen w-screen text-3xl font-bold text-gray-200'>
+      Hello, Gaushala Collection Admin Panel!
+    </div>
+    </>
+  )
+}
+
+export default App
