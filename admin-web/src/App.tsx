96584fdfa376fc3d4ace43800bb973697d1c7464
jagjeet147 <jagjeet695446@gmail.com>
2026-01-27 09:58:15 +0530

    first commit

--- admin-web/src/App.tsx
+++ admin-web/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-    <div className=' flex bg-gray-900 justify-center items-center h-screen w-screen text-3xl font-bold text-gray-200'>
+    <div className=' flex bg-gray-900  justify-center items-center h-screen w-screen text-3xl font-bold text-gray-200'>
       Hello, Gaushala Collection Admin Panel!
     </div>
     </>
